be028925ebec31da65b7005e43cdafa0e4ed666b
KUMIMI123 <tiqkd433@naver.com>
2021-02-22 01:48:23 +0900

    2021.02.22

---
+++ portfolo-mimi/index.js
@@ -0,0 +1,20 @@
+<!--
+function openPop()
+{
+window.open("https://raw.githubusercontent.com/KUMIMI123/my-img/master/portfolio/%EC%B1%85%ED%91%9C%EC%A7%80%20%EC%8B%9C%EC%95%88%EC%99%84%EC%84%B1%20-%20%EB%B3%B5%EC%82%AC%EB%B3%B8.jpg", "startpop", "top=0, left=0, width=981, height=736, scrollbars=yes, resizable=no ,status=no ,toolbar=no");
+}
+//-->
+
+<!--
+function openPop2()
+{
+window.open("https://raw.githubusercontent.com/KUMIMI123/my-img/master/portfolio/ux%EC%9E%91%EC%97%85%EC%99%84%EB%A3%8C(%EC%A7%84%EC%A7%9C%20%EC%B5%9C%EC%A2%85)%20-%20%EB%B3%B5%EC%82%AC%EB%B3%B8.jpg", "startpop", "top=0, left=0, width=670, height=754, scrollbars=yes, resizable=no ,status=no ,toolbar=no");
+}
+//-->
+
+<!--
+function openPop4()
+{
+window.open("https://raw.githubusercontent.com/KUMIMI123/my-img/master/portfolio/%ED%8F%AC%ED%8F%B4%EB%A1%9C%EA%B3%A0%EC%8B%9C%EC%95%88%EC%99%84%EC%84%B1%20-%20%EB%B3%B5%EC%82%AC%EB%B3%B8.jpg", "top=0, left=0, width=670, height=754, scrollbars=yes, resizable=no ,status=no ,toolbar=no");
+}
+//-->
